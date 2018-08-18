d4cdaeca2bc3521856f20cd67e4d0d387f29773d
Kenneth Duverge <duvergetech@gmail.com>
2018-08-18 12:16:31 -0400

    updates

--- index.js
+++ index.js
@@ -7,21 +7,36 @@
   let queue = [document.body][0].childNodes;
   let curr;
 
+  let elements = ["home", "product", "category", "search"];
+
+  elements.forEach((el, i) => {
+    localStorage.setItem(el, i);
+  });
+
+  class scrapedValues {
+    constructor(el, word, id, className) {
+      this.el = el;
+      this.word = word;
+      this.id = id;
+      this.className = className;
+    }
+  }
+
   let foundWord = [];
   console.log(queue);
   queue.forEach((el, i) => {
     if (el.textContent.toLowerCase().includes(word) || el.className === word) {
-      console.log(el.textContent);
+      console.log(el);
       foundWord.push(el.className);
       foundWord.push(el.textContent);
     }
   });
+  let home = localStorage.getItem(word);
 
-  console.log(foundWord);
+  console.log(home);
+  // console.log(foundWord);
   input.addEventListener("change", function(e) {
     keyWord = e.target.value;
   });
-  button.addEventListener("click", function() {
-    console.log(keyWord);
-  });
+  button.addEventListener("click", function() {});
 })();
